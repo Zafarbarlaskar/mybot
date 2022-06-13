179c06803bb92df16937330b9210391511f56104
Zafarbarlaskar <96040383+Zafarbarlaskar@users.noreply.github.com>
2022-06-13 20:18:33 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -210,8 +210,8 @@ async def help(client, message):
 @Client.on_message(filters.command("about"))
 async def aboutme(client, message):
         buttons= [[
-            InlineKeyboardButton('Github', url='https://github.com/200920082007'),
-            InlineKeyboardButton('𝖬𝗈𝗏𝗂𝖾𝗌', url='https://t.me/MovieHubOfficialGroup'),
+            InlineKeyboardButton('Github', url='https://t.me/Aboutmedaredevil'),
+            InlineKeyboardButton('𝖬𝗈𝗏𝗂𝖾𝗌', url='https://t.me/Movies_corner20'),
             InlineKeyboardButton('Close', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
